e1a543d2223a8a61b9013d99fbc7ad5a3ca10346
Jens Glander <jens.glander@sap.com>
2014-09-21 23:40:03 +0200

    [v-1.4.2] Section 2.2 of PersonsList HANA Cloud Tutorial
    Section 2.2: Add a SAPUI5 button to Application View

--- personslist/personslist-web/WebContent/personslist-web/personslist.view.js
+++ personslist/personslist-web/WebContent/personslist-web/personslist.view.js
@@ -13,7 +13,16 @@ sap.ui.jsview("personslist-web.personslist", {
 	* @memberOf personslist-web.personslist
 	*/ 
 	createContent : function(oController) {
+		// create the button instance
+		var myButton = new sap.ui.commons.Button("btn");
 
+		// set properties, e.g. the text (there is also a shorter way of setting several properties)
+		myButton.setText("Hello World!");
+
+		// attach an action to the button's "press" event (use jQuery to fade out the button)
+		myButton.attachPress(function(){$("#btn").fadeOut();});
+
+		return myButton;
 	}
 
 });
