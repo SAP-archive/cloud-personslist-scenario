83c007103daf61b392f9c533a7108098baebe236
Jens Glander <jens.glander@sap.com>
2014-09-22 00:01:43 +0200

    [v-1.4.2] Extension-001: OData CRUD Sample Web Application
    - This sample is based on Extension-000 sample
    - Added a sapui5 panel area to PersonsList Ui with which a user can
      create, read, update and delete personslist entities.
      These CRUD operations are using the PersonsList OData service. 
    - UI selenium tests for simple PersonsList are deleted
    - published on SCN
    http://scn.sap.com/community/developer-center/cross-technology/blog/2014/01/24/hcp-personslist-sample-extension-001
    
    - ReadMe.txt describes shortly how to run this sample

--- personslist/personslist-web/src/main/webapp/personslist-web/personslist.controller.js
+++ personslist/personslist-web/src/main/webapp/personslist-web/personslist.controller.js
@@ -6,58 +6,173 @@ sap.ui.controller("personslist-web.personslist", {
 * @memberOf personslist-web.personslist
 */
 	onInit : function() {
-		var sOrigin = window.location.protocol + "//" + window.location.hostname
-				+ (window.location.port ? ":" + window.location.port : "");
-		var personsListOdataServiceUrl = sOrigin + "/personslist-repo-web/personslist.svc";
-
-		var odataModel = new sap.ui.model.odata.ODataModel(
-				personsListOdataServiceUrl);
+		var odataModel = new sap.ui.model.odata.ODataModel(this
+				.getPersonsODataServiceURL());
 		odataModel.setCountSupported(false);
 		this.getView().setModel(odataModel);
 	},
 
-	addNewPerson : function(sFirstName, sLastName, oTable) {
-		var persons = {};
-
-		persons.FirstName = sFirstName;
-		persons.LastName = sLastName;
-
-		this.getView().getModel().create("/Persons", persons, null, this.successMsg, this.errorMsg);
+	getPersonsODataServiceURL : function() {
+		var sOrigin = window.location.protocol + "//"
+				+ window.location.hostname
+				+ (window.location.port ? ":" + window.location.port : "");
+		return sOrigin + "/personslist-repo-web/personslist.svc";
 	},
 
-	successMsg : function() {
-		sap.ui.commons.MessageBox.alert("Person entity has been successfully created");
+	// ----------
+	//   CREATE
+	// ----------
+	createPerson : function(oTable) {
+		var createFirstName = this.getView().getFirstNameField().getValue();
+		var createLastName = this.getView().getLastNameField().getValue();
+
+		// empty field check
+		if (createFirstName === "" || createLastName === "") {
+			this.showMsgArea("warning", "Enter non-empty 'First Name' and 'Last Name' values before you create a person.");
+		} else {
+			var persons = {};
+			persons.FirstName = createFirstName;
+			persons.LastName = createLastName;
+
+			oTable.clearSelection();
+
+			this.getView().getModel().create(
+					"/Persons", 
+					persons, 
+					null,
+					jQuery.proxy(
+							function(oData) {
+								this.showMsgArea("success", "Person entity has been successfully created");
+							}, this), 
+					jQuery.proxy(
+							function(oData) {
+								this.showMsgArea("error", "Error occured while creating person entity");
+							}, this)
+			);
+			// clear fields
+			this.getView().getFirstNameField().unbindElement().setValue("");
+			this.getView().getLastNameField().unbindElement().setValue("");
+		}
 	},
 
-	errorMsg : function() {
-		sap.ui.commons.MessageBox.alert("Error occured when creating person entity");
+	// ----------
+	//   READ
+	// ----------
+	readPersons : function(){
+		this.getView().getModel().read(
+				"/Persons", 
+				null,
+				null,
+				true,
+				jQuery.proxy(
+						function(oData) {
+							this.showMsgArea("info", oData.results.length + " person(s) currently stored");
+						}, this), 
+				jQuery.proxy(
+						function(oData) {
+							this.showMsgArea("error", "Error occured while creating person entity");
+						}, this)
+		);
 	},
 
+	// ----------
+	//   UPDATE
+	// ----------
+	updateSelectedPerson : function(oTable){
+		var oModel = this.getView().getModel();
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf personslist-web.personslist
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+		// empty table check
+		if (Object.keys(oModel.oData).length<1) {
+			this.showMsgArea("warning", "Table is empty. Create a person table entry before you update a person.");
+		} else {
+			var selectedIndex = oTable.getSelectedIndex();
+			// selected table row
+			if(selectedIndex===-1 ){
+				this.showMsgArea("warning", "Select the table row of the person you want to update.");
+			} else{
+				var oSelectedRowCtx = oTable.getContextByIndex(selectedIndex);
+				var oModifData = oModel.getProperty("", oSelectedRowCtx, false);
+				var updateFirstName = this.getView().getFirstNameField().getValue();
+				var updateLastName = this.getView().getLastNameField().getValue();
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf personslist-web.personslist
-*/
-//	onAfterRendering: function() {
-//
-//	},
+				// empty field check
+				if (updateFirstName === "" || updateLastName === "") {
+					this.showMsgArea("warning", "Enter non-empty 'First Name' and 'Last Name' values to update a person values.");
+				} else {
+					oModifData.FirstName = updateFirstName;
+					oModifData.LastName = updateLastName;
+					oModel.update(
+						oSelectedRowCtx.getPath(), 
+						oModifData, 
+						null, 
+						jQuery.proxy(
+								function(oData) {
+									this.showMsgArea("success", "Person entity has been successfully updated");
+								}, this), 
+						jQuery.proxy(
+								function(oData) {
+									this.showMsgArea("error", "Error occured while updating person entity");
+								}, this), 
+						true
+					);
+				}
+			}
+		}
+	},
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf personslist-web.personslist
-*/
-//	onExit: function() {
-//
-//	}
+	// ----------
+	//   DELETE
+	// ----------
+	deleteSelectedPerson : function(oTable){
+		var oModel = this.getView().getModel();
+
+		// empty table check
+		if (Object.keys(oModel.oData).length<1) {
+			this.showMsgArea("warning", "Table is empty. Create a person table entry before you delete a person.");
+		} else {
+			var selectedIndex = oTable.getSelectedIndex();
+			// selected table row
+			if(selectedIndex===-1 ){
+				this.showMsgArea("warning", "Select the table row of the person you want to delete.");
+			} else{
+				var oSelectedRowCtx = oTable.getContextByIndex(selectedIndex);
+				oTable.clearSelection();
+				oModel.remove(
+						'', 
+						oSelectedRowCtx, 
+						jQuery.proxy(
+								function(oData) {
+									this.showMsgArea("success", "Person entity has been successfully deleted");
+								}, this), 
+						jQuery.proxy(
+								function(oData) {
+									this.showMsgArea("error", "Error occured while deleting person entity");
+								}, this) 
+				);
+				// clear fields
+				this.getView().getFirstNameField().unbindElement().setValue("");
+				this.getView().getLastNameField().unbindElement().setValue("");
+			}
+		}
+	},
+
+	// -------------------------
+	//   message area handling
+	// -------------------------
+	hideMsgArea : function() {
+		this.getView().getMsgField().setText("");
+		this.getView().getMsgViewHLayout().setVisible(false);
+	},
+
+	showMsgArea : function(iconType, msgText) {
+		this.getView().getMsgField().setText(msgText);
+		if (iconType === "info" || iconType === "warning" || iconType === "error" || iconType === "success") {
+			this.getView().getMsgIcon().setSrc("images/" + iconType + ".png").setVisible(true);
+		} else {
+			this.getView().getMsgViewHLayout().setVisible(false);
+		}
+		this.getView().getMsgViewHLayout().setVisible(true);
+		this.getView().getCrudPanel().setCollapsed(false);
+	}
 
 });
--- personslist/personslist-web/src/main/webapp/personslist-web/personslist.view.js
+++ personslist/personslist-web/src/main/webapp/personslist-web/personslist.view.js
@@ -1,5 +1,12 @@
 sap.ui.jsview("personslist-web.personslist", {
 
+	oFirstNameField : null,
+	oLastNameField : null,
+	oCrudPanel : null,
+	oMsgViewHLayout : null,
+	oMsgField : null,
+	oMsgIcon : null,
+
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
 	* @memberOf personslist-web.personslist
@@ -12,88 +19,234 @@ sap.ui.jsview("personslist-web.personslist", {
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
 	* @memberOf personslist-web.personslist
 	*/ 
-    createContent : function(oController) {
-  		// Create an instance of the table control
-  		var oTable = new sap.ui.table.Table({
-  			title : "Persons List",
-  			visibleRowCount : 7,
-  			firstVisibleRow : 3,
-  			selectionMode : sap.ui.table.SelectionMode.Single,
-  		});
 
-  		// toolbar
-  		var oTableToolbar = new sap.ui.commons.Toolbar();
+	createContent : function(oController) {
+		var that = this;
+		// -----------------------
+		//   PERSONS TABLE panel
+		// -----------------------
+		var oPersonsTable = new sap.ui.table.Table({
+			visibleRowCount : 7,
+			selectionMode : sap.ui.table.SelectionMode.Single,
+			rowSelectionChange : function(oEvent) {
+				var oRowCtx = oEvent.getParameters().rowContext;
+				if(oRowCtx!==null){ // in case of table.clearSelection() it is null
+					that.oFirstNameField.bindElement(oRowCtx.getPath());
+					that.oLastNameField.bindElement(oRowCtx.getPath());
+				}
+			}
+		});
 
-  		// first name field
-  		var oFirstNameLabel = new sap.ui.commons.Label({
-  			text : 'First Name'
-  		});
-  		var oFirstNameField = new sap.ui.commons.TextField({
-  			id : 'firstNameFieldId',
-  			value : '',
-  			width : '10em',
-  		});
-  		oFirstNameLabel.setLabelFor(oFirstNameField);
+		// define the columns and the control templates to be used
+		oPersonsTable.addColumn(new sap.ui.table.Column({
+			label : new sap.ui.commons.Label({
+				text : "First Name"
+			}),
+			template : new sap.ui.commons.TextField().bindProperty("value", "FirstName"),
+			sortProperty : "FirstName",
+			filterProperty : "FirstName",
+			width : "20%"
+		}));
+		oPersonsTable.addColumn(new sap.ui.table.Column({
+			label : new sap.ui.commons.Label({
+				text : "Last Name"
+			}),
+			template : new sap.ui.commons.TextField().bindProperty("value", "LastName"),
+			sortProperty : "LastName",
+			filterProperty : "LastName",
+			width : "80%"
+		}));
 
-  		oTableToolbar.addItem(oFirstNameLabel);
-  		oTableToolbar.addItem(oFirstNameField);
+		// bind table rows to /Persons based on the model defined in the init method of the controller
+		oPersonsTable.bindRows("/Persons");
 
-  		// last name field
-  		var oLastNameLabel = new sap.ui.commons.Label({
-  			text : 'Last Name'
-  		});
-  		var oLastNameField = new sap.ui.commons.TextField({
-  			id : 'lastNameFieldId',
-  			value : '',
-  			width : '10em',
-  		});
-  		oLastNameLabel.setLabelFor(oLastNameField);
+		// --------------
+		//   CRUD panel
+		// --------------
+		this.oCrudPanel = new sap.ui.commons.Panel({
+			title : new sap.ui.core.Title({
+				text : "CRUD OData Service Example"
+			})
+		}).addStyleClass("padding-top-10px");
 
-  		oTableToolbar.addItem(oLastNameLabel);
-  		oTableToolbar.addItem(oLastNameField);
+		// -----------------
+		//   CREATE button
+		// -----------------
+		var oCreateBtn = new sap.ui.commons.Button({
+			text : "Create",
+			tooltip : "Create a new person entity with the entered first and last name",
+			style : sap.ui.commons.ButtonStyle.Default,
+			press : function() {
+				oController.createPerson(oPersonsTable);
+			}
+		});
 
-  		// add button
-  		var oAddPersonButton = new sap.ui.commons.Button({
-  			id : 'addPersonButtonId',
-  			text : "Add Person",
-  			press : function() {
-  				oController.addNewPerson(sap.ui.getCore().getControl(
-  						"firstNameFieldId").getValue(), sap.ui.getCore()
-  						.getControl("lastNameFieldId").getValue(), oTable);
-  			}
-  		});
-  		oTableToolbar.addItem(oAddPersonButton);
+		// ---------------
+		//   READ button
+		// ---------------
+		var oReadBtn = new sap.ui.commons.Button({
+			text : "Read",
+			tooltip : "Read persons OData service to retrieve the numbers of stored persons",
+			style : sap.ui.commons.ButtonStyle.Default,
+			press : function() {
+				oController.readPersons();
+			}
+		});
 
-  		oTable.setToolbar(oTableToolbar);
+		// -----------------
+		//   UPDATE button
+		// -----------------
+		var oUpdateBtn = new sap.ui.commons.Button({
+			text : "Update",
+			tooltip : "Select a row from persons table and change the displayed names. Choose update to store the changed values",
+			style : sap.ui.commons.ButtonStyle.Default,
+			press : function() {
+				oController.updateSelectedPerson(oPersonsTable);
+			}
+		});
 
-  		// define the columns and the control templates to be used
-  		oTable.addColumn(new sap.ui.table.Column({
-  			label : new sap.ui.commons.Label({
-  				text : "First Name"
-  			}),
-  			template : new sap.ui.commons.TextField().bindProperty("value",
-  					"FirstName"),
-  			sortProperty : "FirstName",
-  			filterProperty : "FirstName",
-  			width : "100px"
-  		}));
-  		oTable.addColumn(new sap.ui.table.Column({
-  			label : new sap.ui.commons.Label({
-  				text : "Last Name"
-  			}),
-  			template : new sap.ui.commons.TextField().bindProperty("value",
-  					"LastName"),
-  			sortProperty : "LastName",
-  			filterProperty : "LastName",
-  			width : "200px"
-  		}));
+		// -----------------
+		//   DELETE button
+		// -----------------
+		var oDeleteBtn = new sap.ui.commons.Button({
+			text : "Delete",
+			tooltip : "Delete person entity of the selected table row",
+			style : sap.ui.commons.ButtonStyle.Default,
+			press : function() {
+				oController.deleteSelectedPerson(oPersonsTable);
+			}
+		});
 
+		this.oCrudPanel.addButton(oCreateBtn);
+		this.oCrudPanel.addButton(oReadBtn);
+		this.oCrudPanel.addButton(oUpdateBtn);
+		this.oCrudPanel.addButton(oDeleteBtn);
 
- // bind table rows to /Persons based on the model defined in the init method of the controller
-  		oTable.bindRows("/Persons");
+		// ----------------------
+		//   CRUD panel content
+		// ----------------------
+		// firstname field
+		var oFirstNameLabel = new sap.ui.commons.Label({
+			text : 'First Name'
+		}).addStyleClass("padding-3px-5px-3px-0px");
+		this.oFirstNameField = new sap.ui.commons.TextField({
+			value : "{FirstName}"
+		});
+		oFirstNameLabel.setLabelFor(this.oFirstNameField);
 
-  		return oTable;
-      }
+		var oFirstNameHLayout = new sap.ui.commons.layout.HorizontalLayout({
+			content : [ oFirstNameLabel, this.oFirstNameField]
+		}).addStyleClass("padding-top-2px");
 
+		// lastname field
+		var oLastNameLabel = new sap.ui.commons.Label({
+			text : 'Last Name'
+		}).addStyleClass("padding-3px-5px-3px-0px");
+		this.oLastNameField = new sap.ui.commons.TextField({
+			value : "{LastName}"
+		});
+		oLastNameLabel.setLabelFor(this.oLastNameField);
+		var oLastNameHLayout = new sap.ui.commons.layout.HorizontalLayout({
+			content : [ oLastNameLabel,this.oLastNameField]
+		}).addStyleClass("padding-top-2px");
+
+		// messages area (only visible if a message is displayed)
+		this.oMsgField = new sap.ui.commons.TextView({
+			text : ""
+		});
+		this.oMsgIcon = new sap.ui.commons.Image({
+			src : "images/info.png",
+			height : "16px",
+			width : "16px"
+		});
+		this.oMsgViewHLayout = new sap.ui.commons.layout.HorizontalLayout({
+			visible : false,
+			content : [
+					this.oMsgIcon.addStyleClass("padding-0px-5px-5px-0px"),
+					this.oMsgField.addStyleClass("padding-top-2px"),
+					new sap.ui.commons.Button({
+						text : "Close Message",
+						tooltip : "Close the displayed message",
+						style : sap.ui.commons.ButtonStyle.Default,
+						press : function() {
+							oController.hideMsgArea();
+						}
+					}).addStyleClass("margin-left-3px")]
+		}).addStyleClass("padding-top-10px");
+
+		var oCrudPanelVLayout = new sap.ui.commons.layout.VerticalLayout({
+			content : [ oFirstNameHLayout, 
+						oLastNameHLayout, 
+						this.oMsgViewHLayout, 
+						new sap.ui.commons.layout.VerticalLayout({
+							content : [ 
+								new sap.ui.commons.TextView({
+									text : "UI5 table control bound to Persons OData Service",
+									design : sap.ui.commons.TextViewDesign.Bold
+								}).addStyleClass("padding-top-30px"), 
+								oPersonsTable ]
+						})]
+		});
+		this.oCrudPanel.addContent(oCrudPanelVLayout);
+
+		var oMainHeader = 
+			new sap.ui.commons.layout.VerticalLayout({
+				content : [
+				new sap.ui.commons.layout.HorizontalLayout({
+					content : [ 
+					new sap.ui.commons.Image({
+						src : "images/SAPLogo.gif",
+						height : "16px",
+						width : "32px"
+					}).addStyleClass("padding-0px-5px-5px-0px"),
+					new sap.ui.commons.TextView({
+						text : "HANA Cloud Development Example",
+						design : sap.ui.commons.TextViewDesign.H3
+					})]
+				}),
+				new sap.ui.commons.layout.HorizontalLayout({
+					content : [ 
+					new sap.ui.commons.TextView({
+						text : "CRUD Web Application Example using",
+						design : sap.ui.commons.TextViewDesign.Standard
+					}), 
+					new sap.ui.commons.Link({
+						text : "Persons OData Serivce",
+						href : oController.getPersonsODataServiceURL(),
+						target : "_blank"
+					}).addStyleClass("margin-left-3px") ]
+				})]
+			}).addStyleClass("padding-5px-0px-10px-2px");
+
+		var oMainLayout = new sap.ui.commons.layout.VerticalLayout({
+			content : [ oMainHeader, this.oCrudPanel ]
+		}).addStyleClass("margin-8px");
+
+		return oMainLayout;
+	},
+
+	getFirstNameField : function() {
+		return this.oFirstNameField;
+	},
+
+	getLastNameField : function() {
+		return this.oLastNameField;
+	},
+
+	getCrudPanel : function() {
+		return this.oCrudPanel;
+	},
+
+	getMsgViewHLayout : function() {
+		return this.oMsgViewHLayout;
+	},
+
+	getMsgField : function() {
+		return this.oMsgField;
+	},
+
+	getMsgIcon : function() {
+		return this.oMsgIcon;
+	}
 
 });
