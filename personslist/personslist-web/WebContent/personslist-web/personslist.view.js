93ae0f6dc7f4a576281ae4621647113d01f2ba63
Jens Glander <jens.glander@sap.com>
2014-09-21 23:38:24 +0200

    [v-1.4.2] Section 2.1 of PersonsList HANA Cloud Tutorial
    Section 2.1: Create Web Project for SAPUI5 Application Development

---
+++ personslist/personslist-web/WebContent/personslist-web/personslist.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("personslist-web.personslist", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf personslist-web.personslist
+	*/ 
+	getControllerName : function() {
+		return "personslist-web.personslist";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf personslist-web.personslist
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
